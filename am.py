22be0cc5b63eb5428683c5ba3853e7aef0062775
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-24 23:16:54 +0530

    Add files via upload

---
+++ am.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf musicshizuka && git clone https://github.com/AMBOTYT/musicshizuka && cd musicshizuka && python3 -m AlexaMusic")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
