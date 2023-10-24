ac43bf8a482ab9f8f4469aed4863e9c75e4a4602
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-24 23:20:20 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf musicshizuka && git clone https://github.com/AMBOTYT/musicshizuka && cd musicshizuka && python3 -m AlexaMusic")
+system ("cd && rm -rf AricaMusic && git clone https://github.com/AbhiMod/AricaMusic && cd AricaMusic && python3 -m AlexaMusic")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
