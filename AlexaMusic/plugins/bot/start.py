9d71c4fdd7d4e6ded535e30fae9f6194d32e4335
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-28 12:18:24 +0530

    Update start.py

--- AlexaMusic/plugins/bot/start.py
+++ AlexaMusic/plugins/bot/start.py
@@ -106,7 +106,7 @@ async def start_comm(client, message: Message, _):
             await sudoers_list(client=client, message=message, _=_)
             if await is_on_off(config.LOG):
                 sender_id = message.from_user.id
-                sender_name = message.from_user.first_name
+                sender_name = message.from_user.mention
                 return await app.send_message(
                     config.LOG_GROUP_ID,
                     f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <code>sᴜᴅᴏʟɪsᴛ</code>\n\n**ᴜsᴇʀ ɪᴅ:** {sender_id}\n**ᴜsᴇʀɴᴀᴍᴇ:** {sender_name}",
@@ -178,22 +178,22 @@ async def start_comm(client, message: Message, _):
             OWNER = OWNER_ID[0]
         except:
             OWNER = None
-        out = private_panel(_, app.mention, OWNER)
+        out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
-                    caption=_["start_2"].format(message.from_user.mention,app.mention),
+                    caption=_["start_2"].format(message.from_user.mention,app.username),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
             except:
                 await message.reply_text(
-                    _["start_2"].format(message.from_user.mention,app.mention),
+                    _["start_2"].format(message.from_user.mention,app.username),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
         else:
             await message.reply_text(
-                _["start_2"].format(message.from_user.mention,app.mention),
+                _["start_2"].format(message.from_user.mention,app.username),
                 reply_markup=InlineKeyboardMarkup(out),
             )
         if await is_on_off(config.LOG):
@@ -215,7 +215,7 @@ async def start_comm(client, message: Message, _):
 async def testbot(client, message: Message, _):
     out = start_pannel(_)
     return await message.reply_text(
-        _["start_1"].format(message.chat.title, app.mention),
+        _["start_1"].format(message.chat.title, app.username),
         reply_markup=InlineKeyboardMarkup(out),
     )
 
